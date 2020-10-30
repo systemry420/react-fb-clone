fe51e897147d6f43e36787f53adaaba5810f3254
hsn <systemry420gmail.com>
2020-10-30 15:27:51 +0200

    add post component & style it

--- src/comps/Feed.js
+++ src/comps/Feed.js
@@ -4,6 +4,32 @@ import StoryReel from './StoryReel'
 import Form from './Form'
 import Post from './Post'
 
+
+// array to handle posts data
+const data = [
+    {
+        profilePic: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTdX7wWCMOvGYD6_4-MthVKf-DjjgLF_GqQzg&usqp=CAU",
+        message: "baby",
+        time: "time",
+        userName: "friend 1",
+        image: "https://www.imagesjunction.com/images/img/cute_baby_images.jpg"
+    },
+    {
+        profilePic: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTdX7wWCMOvGYD6_4-MthVKf-DjjgLF_GqQzg&usqp=CAU",
+        message: "Galaxy",
+        time: "time",
+        userName: "friend 2",
+        image: "https://www.technocrazed.com/wp-content/uploads/2015/12/HD-Space-Wallpaper-For-Background-22.jpg"
+    },
+    {
+        profilePic: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTdX7wWCMOvGYD6_4-MthVKf-DjjgLF_GqQzg&usqp=CAU",
+        message: "Hello",
+        time: "time",
+        userName: "friend 3",
+        image: ""
+    },
+]
+
 function Feed() {
     return (
         <div className="feed">
@@ -12,7 +38,9 @@ function Feed() {
             {/* form */}
             <Form />
 
-            <Post />
+            {data.map((post, i)=>{
+                return <Post key={i} {...post} />
+            })}
         </div>
     )
 }
--- src/comps/Post.js
+++ src/comps/Post.js
@@ -1,10 +1,46 @@
 import React from 'react'
 import './../App.css'
+import { Avatar } from '@material-ui/core'
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import NearMeIcon from '@material-ui/icons/NearMe';
 
-function Post() {
+function Post({ profilePic, image, userName, time, message}) {
     return (
         <div className="post">
+            <div className="post_top">
+                <Avatar src={profilePic} className="post_avatar" />
+                <div className="post_info">
+                    <h3>{userName}</h3>
+                    <p>{time}</p>
+                </div>
+            </div>
             
+            <div className="post_body">
+                <div className="message">
+                    <p>
+                        {message}
+                    </p>
+                </div>
+                <div className="image">
+                    <img src={image} alt="" />
+                </div>
+            </div>
+
+            <div className="post_bottom">
+                <div className="post_option">
+                    <ThumbUpIcon />
+                    <p>Like</p>
+                </div>
+                <div className="post_option">
+                    <ChatBubbleOutlineIcon />
+                    <p>Comment</p>
+                </div>
+                <div className="post_option">
+                    <NearMeIcon />
+                    <p>Share</p>
+                </div>
+            </div>
         </div>
     )
 }
